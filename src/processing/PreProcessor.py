9a7bad60ee2f49ec78ed3b5c2c2396c4254dbf2d
Marcel Bergmann <marcel.bergmann@capgemini.com>
2020-05-18 14:19:58 +0200

    Adding validation set to preprocessor

--- src/processing/PreProcessor.py
+++ src/processing/PreProcessor.py
@@ -6,33 +6,55 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 class PreProcessor:
 
-    def __init__(self, sentences, ner_tags, oov_token: str = "<OOV>"):
+    def __init__(self, sentences, ner_tags, val_sentences, val_ner_tags, oov_token: str = "<OOV>"):
         self._sentences = sentences
         self._ner_tags = ner_tags
+        self._val_sentences = val_sentences
+        self._val_ner_tags = val_ner_tags
+
         self._input_sequences = None
         self._label_sequences = None
+        self._val_input_sequences = None
+        self._val_label_sequences = None
+
         self._tokenizer = Tokenizer(oov_token=oov_token)
         self._label_tokenizer = Tokenizer()
+
         self._max_sequence_length = None
 
     def pre_process_data(self):
-        self._pre_process_input_sequences()
-        self._pre_process_label_sequences()
+        self._pre_process_train_input_sequences()
+        self._pre_process_train_label_sequences()
 
-    def _pre_process_input_sequences(self):
-        self._tokenizer.fit_on_texts(self._sentences)
-        input_sequences = self._tokenizer.texts_to_sequences(self._sentences)
-        padded_input_sequences = pad_sequences(input_sequences, padding='post')
+        self._pre_process_validation_input_sequences()
+        self._pre_process_validation_label_sequences()
 
-        self._compute_max_sequence_len(input_sequences)
-        self._input_sequences = np.array(padded_input_sequences)
+    def _pre_process_train_input_sequences(self):
+        self._input_sequences = self._pre_process_input_sequence(self._sentences)
 
-    def _pre_process_label_sequences(self):
-        self._label_tokenizer.fit_on_texts(self._ner_tags)
-        label_sequences = self._label_tokenizer.texts_to_sequences(self._ner_tags)
-        padded_label_sequences = pad_sequences(label_sequences, padding='post')
+    def _pre_process_train_label_sequences(self):
+        self._label_sequences = self._pre_process_label_sequences(self._ner_tags)
 
-        self._label_sequences = np.array(padded_label_sequences)
+    def _pre_process_validation_input_sequences(self):
+        self._val_input_sequences = self._pre_process_input_sequence(self._val_sentences, validation=True)
+
+    def _pre_process_validation_label_sequences(self):
+        self._val_label_sequences = self._pre_process_label_sequences(self._val_ner_tags, validation=True)
+
+    def _pre_process_input_sequence(self, sentences, validation=False):
+        if not validation:
+            self._tokenizer.fit_on_texts(sentences)
+            self._compute_max_sequence_len(sentences)
+        input_sequences = self._tokenizer.texts_to_sequences(sentences)
+        padded_input_sequences = pad_sequences(input_sequences, padding='post', maxlen=self._max_sequence_length)
+        return np.array(padded_input_sequences)
+
+    def _pre_process_label_sequences(self, ner_tags, validation=False):
+        if not validation:
+            self._label_tokenizer.fit_on_texts(ner_tags)
+        label_sequences = self._label_tokenizer.texts_to_sequences(ner_tags)
+        padded_label_sequences = pad_sequences(label_sequences, padding='post', maxlen=self._max_sequence_length)
+        return np.array(padded_label_sequences)
 
     def _compute_max_sequence_len(self, input_sequences):
         seq_lengths = [len(seq) for seq in input_sequences]
@@ -45,6 +67,14 @@ class PreProcessor:
     @property
     def label_sequences(self):
         return self._label_sequences
+
+    @property
+    def val_input_sequences(self):
+        return self._val_input_sequences
+
+    @property
+    def val_label_sequences(self):
+        return self._val_label_sequences
 
     @property
     def num_unique_word_tokens(self):
--- test/processing/test_pre_processor.py
+++ test/processing/test_pre_processor.py
@@ -1,8 +1,10 @@
 import numpy as np
+
+from numpy.testing import assert_array_equal
 from src.processing.PreProcessor import PreProcessor
 
 
-def test_pre_process_data():
+def test_pre_process_data_training():
     in_sentences = [
         ['SOCCER', '-', 'JAPAN', 'GET', 'LUCKY', 'WIN', ',', 'CHINA', 'IN', 'SURPRISE',
          'DEFEAT', '.'],
@@ -14,7 +16,7 @@ def test_pre_process_data():
         ['I-PER', 'I-PER']
     ]
 
-    pre_processor = PreProcessor(in_sentences, in_tags)
+    pre_processor = PreProcessor(in_sentences, in_tags, in_sentences, in_tags)
     pre_processor.pre_process_data()
 
     expected_input_sequences = np.array([
@@ -31,7 +33,37 @@ def test_pre_process_data():
 
     expected_num_unique_label_tokens = 3
 
-    assert (pre_processor.input_sequences == expected_input_sequences).all()
-    assert (pre_processor.label_sequences == expected_label_sequences).all()
+    assert_array_equal(pre_processor.input_sequences, expected_input_sequences)
+    assert_array_equal(pre_processor.label_sequences, expected_label_sequences)
+
     assert pre_processor.num_unique_word_tokens == expected_num_unique_word_tokens
     assert pre_processor.num_unique_label_tokens == expected_num_unique_label_tokens
+
+
+def test_pre_process_data_validation():
+    in_sentences = [
+        ['SOCCER', '-', 'JAPAN', 'GET', 'LUCKY', 'WIN', ',', 'CHINA', 'IN', 'SURPRISE',
+         'DEFEAT', '.'],
+        ['Nadim', 'Ladki']
+    ]
+
+    in_tags = [
+        ['O', 'O', 'I-LOC', 'O', 'O', 'O', 'O', 'I-PER', 'O', 'O', 'O', 'O'],
+        ['I-PER', 'I-PER']
+    ]
+
+    pre_processor = PreProcessor(in_sentences, in_tags, in_sentences, in_tags)
+    pre_processor.pre_process_data()
+
+    expected_input_sequences = np.array([
+        [2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13],
+        [14, 15, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    ])
+
+    expected_label_sequences = np.array([
+        [1, 1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1],
+        [2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+
+    assert_array_equal(pre_processor.val_input_sequences, expected_input_sequences)
+    assert_array_equal(pre_processor.val_label_sequences, expected_label_sequences)
